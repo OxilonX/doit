6c264340312f91ab8fecbab3595778c8bb2d7056
oxilon <boabmeuni01@gmail.com>
2025-06-19 20:43:41 +0100

    resoponsive to-do list completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,31 @@
 const addButton = document.querySelector(".add-button");
+const addIcon = document.querySelector(".add-icon");
 const deleteButton = document.querySelector(".delete-icon");
 const input = document.querySelector(".task-input");
+const themesIcon = document.querySelector(".themes-icon");
 let allTasks = [];
 let tasksContainer = document.querySelector(".tasks-container");
 let task = document.querySelector(".task-container");
 let i = 0;
 function addTask() {
   const taskContent = input.value.trim();
-
   if (taskContent) {
-    i++;
-    const taskHTML = ` <div class="task-container ">
-            <div class="inset-task">
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-              </label>
-              <div class="task">${taskContent}</div>
-            </div>
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
 
-            <img
-              src="icons/icons8-delete-60-black.png"
-              alt=""
-              class="delete-icon "
-            />
-          </div>`;
+    taskContainer.innerHTML = `
+      <div class="inset-task">
+        <label class="checkbox">
+          <input type="checkbox" />
+          <span class="checkmark"></span>
+        </label>
+        <div class="task">${taskContent}</div>
+      </div>
+      <img src="icons/icons8-delete-60-black.png" alt="" class="delete-icon" />
+    `;
 
-    allTasks.push(taskHTML);
+    tasksContainer.appendChild(taskContainer);
     input.value = "";
-    tasksContainer.innerHTML += taskHTML;
   }
 }
 function deleteTask() {
@@ -41,7 +38,50 @@ function deleteTask() {
     }
   });
 }
-deleteTask();
+function changeVar(variable, value) {
+  document.documentElement.style.setProperty(variable, value);
+}
+themesIcon.onclick = () => {
+  const isMoonIcon = themesIcon.src.includes("icons/icons8-moon-30.png");
+  const testDelete = document.querySelector(".delete-icon");
+  if (isMoonIcon) {
+    themesIcon.src = "icons/icons8-sun-30.png";
+    addIcon.src = "icons/icons8-add-30.png";
+
+    changeVar("--color", "#ff5733");
+    changeVar("--bgc", "#000000");
+    changeVar("--sec-color", "#ffffff");
+
+    updateDeleteIcons("icons/icons8-delete-60.png");
+  } else {
+    themesIcon.src = "icons/icons8-moon-30.png";
+    addIcon.src = "icons/icons8-add-30-black.png";
+
+    changeVar("--color", "#ff5733");
+    changeVar("--bgc", "#ffffff");
+    changeVar("--sec-color", "#000000");
+    updateDeleteIcons("icons/icons8-delete-60-black.png");
+  }
+};
+
+function updateDeleteIcons(src) {
+  const deleteIcons = document.querySelectorAll(".delete-icon");
+  deleteIcons.forEach((icon) => {
+    icon.src = src;
+  });
+}
+function updateAddButton() {
+  if (window.matchMedia("(max-width: 460px)").matches) {
+    addButton.innerHTML = `<img class="add-icon" src="icons/icons8-add-30-black.png" alt="" />`;
+  } else {
+    addButton.innerHTML = `<img class="add-icon" src="icons/icons8-add-30-black.png" alt="" /> add task`;
+  }
+}
+
+updateAddButton();
+
+window.addEventListener("resize", updateAddButton);
 addButton.onclick = () => {
   addTask();
 };
+deleteTask();
